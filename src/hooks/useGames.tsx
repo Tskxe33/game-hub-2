a4593e14bed452b73b88924c9b8516b3391d7764
Petro Arva <petroarva33@gmail.com>
2024-01-15 17:52:24 +0100

    Update custom hook for fetching games

--- src/hooks/useGames.tsx
+++ src/hooks/useGames.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import apiClient from "../services/api-client";
-
+import { CanceledError } from "axios";
 interface Game {
   id: number;
   name: string;
@@ -16,10 +16,17 @@ const useGames = () => {
   const [error, setError] = useState("");
 
   useEffect(() => {
+    const controller = new AbortController();
+
     apiClient
-      .get<FetchGamesResponse>("/games")
+      .get<FetchGamesResponse>("/games", { signal: controller.signal })
       .then((res) => setGames(res.data.results))
-      .catch((err) => setError(err.message));
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+      });
+
+    return () => controller.abort();
   }, []);
 
   return {
